c0f92eab400f1737b072c5c2fa91b8e98ba54dc9
kanna-7 <vithanalapraveen069@gmail.com>
2025-03-25 14:45:45 +0530

    kj

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
-const express = require('express');
-const { resolve } = require('path');
 
+const express = require('express');
+const mongoose = require('mongoose');
+const { resolve } = require('path');
+const dotenv = require('dotenv')
+const userSchema = require('./Schema')
+dotenv.config();
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.post('/menu',async(req,res)=>{
+  try{
+    const {name,description,price}=req.body;
+    if(!name || !price){
+      res.status(400).send({msg:"All fields are required"});
+    }
+    const data = new userSchema({name,description,price});
+    await data.save();
+    res.status(200).send({msg:"Item created successfully"});
+  }
+  catch (error) {
+    res.status(500).send({msg:"Something went wrong"});
+  }
+})
+app.get('/menu',async (req,res)=>{
+  try {
+    const data = await userSchema.find();
+    res.status(200).json(data);
+  } catch(error){
+    res.status(500).send({msg:'Something went wrong'})
+  }
+})
+app.listen(port, async() => {
+  try{
+    await mongoose.connect(process.env.MONGO);
+    console.log('Server connected successfully')
+    console.log(`Example app listening at http://localhost:${port}`);
+  } catch (error){
+    console.log("Error",error);
+  }
+  
 });
